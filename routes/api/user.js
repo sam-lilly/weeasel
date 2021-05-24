76b13fb97534200c983b69bf58557760e8056d14
juliajykim <jkim24@gwu.edu>
2021-05-24 15:22:48 -0400

    make changes on user routes

--- routes/api/user.js
+++ routes/api/user.js
@@ -70,22 +70,16 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = {
-              id: user.id,
-              username: user.username,
-              email: user.email
-            };
+            const payload = { id: user.id, username: user.username, email: user.email };
 
             jwt.sign(
               payload,
               keys.secretOrKey,
               { expiresIn: 3600 },
               (err, token) => {
-                res.json({
-                  success: true,
-                  token: 'Bearer ' + token
-                });
-              });
+                res.json({ success: true, token: 'Bearer ' + token });
+              }
+            );
           } else {
             return res.status(400).json({ password: 'Incorrect password' });
           }
