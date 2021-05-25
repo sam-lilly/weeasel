6174199eccf4ef8e71083728b04e62a1c3fe6dcc
Samantha Lilly <samanthalillydesign@gmail.com>
2021-05-24 21:31:26 -0400

    Add component skeleton

---
+++ frontend/src/components/splash/splash_container.jsx
@@ -0,0 +1,15 @@
+// import { connect } from 'react-redux';
+// import SplashPage from './splash';
+
+// const mSTP = (state) => ({
+//     ???
+// })
+
+// const mDTP = (dispatch) => ({
+//     ???
+// })
+
+// export default connect (mSTP, mDTP)(SplashPage);
+
+// * not sure if we need this or will want this
+// will depend how we set it up but here just in case!
