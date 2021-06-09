52d81bc1af746e79f0772dc05c3c9374c31ed319
Robby Balistreri <rbalistreri5@gmail.com>
2021-06-09 17:09:46 -0500

    Prevent defaults removed

--- frontend/src/components/drawing_boards/drawing_board_index.jsx
+++ frontend/src/components/drawing_boards/drawing_board_index.jsx
@@ -55,7 +55,7 @@ class DrawingBoardIndex extends React.Component {
    }
 
    closeClick(e) {
-      e.preventDefault();
+      // e.preventDefault();
       e.stopPropagation();
       this.setState({
          isOpen: false
